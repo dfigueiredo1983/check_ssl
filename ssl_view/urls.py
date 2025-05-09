0ad49b7ff2e934dee012e31f1498af56589f026b
DISIS-Ibram <gesis.ibram@gmail.com>
2025-05-09 15:39:23 -0300

    Create App ssl_view para mostrar o status dos certificados SSL dos sites do IBRAM

--- ssl_view/urls.py
+++ ssl_view/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Certificate_SSL_View
+from .views import ListSSLView
+from .views import ssl_view
 
 urlpatterns = [
-    path('ssl/', Certificate_SSL_View.as_view()),
+    path('ssl_view/', ListSSLView.as_view()),
+    path('ssl/', ssl_view, name='ssh_view'),
 ]
--- utils/check_ssl.py
+++ utils/check_ssl.py
@@ -10,8 +10,6 @@ def verificar_certificado_ssl(url):
         cert_pem = ssl.get_server_certificate((hostname, port))
         cert = crypto.load_certificate(crypto.FILETYPE_PEM, cert_pem)
 
-        print(type(cert))
-
         # Data de expiração (em bytes) → converter para datetime com timezone UTC
         data_exp_bytes = cert.get_notAfter()
         data_expiracao = datetime.strptime(data_exp_bytes.decode('ascii'), "%Y%m%d%H%M%SZ")
@@ -21,9 +19,11 @@ def verificar_certificado_ssl(url):
 
         if data_atual < data_expiracao:
             dias_expirar = (data_expiracao - data_atual).days
-            return f"[OK] Certificado SSL de '{url}' é válido até {data_expiracao.strftime('%Y-%m-%d %H:%M:%S %Z')} - Dias para expirar: {dias_expirar}"
+            # return f"[OK] - '{url}' é válido até {data_expiracao.strftime('%Y-%m-%d %H:%M:%S %Z')} - Dias para expirar: {dias_expirar}"
+            return f"[OK] - '{url}' é válido até {data_expiracao.strftime('%Y-%m-%d')} - Dias para expirar: {dias_expirar}"
         else:
-            return f"[ERRO] Certificado SSL de '{url}' expirou em {data_expiracao.strftime('%Y-%m-%d %H:%M:%S %Z')}."
+            # return f"[ERRO] - '{url}' expirou em {data_expiracao.strftime('%Y-%m-%d %H:%M:%S %Z')}."
+            return f"[ERRO] - '{url}' expirou em {data_expiracao.strftime('%Y-%m-%d')}."
     except Exception as e:
         return f"[FALHA] Não foi possível verificar o certificado SSL de '{url}'. Erro: {e}"
 
@@ -32,11 +32,11 @@ if __name__ == "__main__":
 
     sites = [
         'https://www.google.com',
-    #     'https://brasiliaambiental.df.gov.br/',
-    #     'https://ibram.df.gov.br/',
-    #     'https://onda.ibram.df.gov.br/portal/home/index.html',
-    #     'https://ondalab.ibram.df.gov.br/portal/home/index.html',
-    #     'https://www.ibram.df.gov.br/harpia/'
+        'https://brasiliaambiental.df.gov.br/',
+        'https://ibram.df.gov.br/',
+        'https://onda.ibram.df.gov.br/portal/home/index.html',
+        'https://ondalab.ibram.df.gov.br/portal/home/index.html',
+        'https://www.ibram.df.gov.br/harpia/'
     ]
 
     for site in sites:
