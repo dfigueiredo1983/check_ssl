c498c81afd399f84ecb2995699425f2ec2e184fa
root <root@DESKTOP-V33KBSN.>
2025-05-09 22:24:12 -0300

    Atualizando

--- ssl_view/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SslViewConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ssl_view'
--- ssl_view/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ListSSLView
-from .views import ssl_view
-
-urlpatterns = [
-    path('ssl_view/', ListSSLView.as_view()),
-    path('ssl/', ssl_view, name='ssh_view'),
-]
